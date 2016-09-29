51c55467e3f3f5dca614c7b38045c55864b2d402
Stephen Dawes <sdawes@outlook.com>
2016-09-29 18:25:53 +0100

    add todo_list items and added delete functionality

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,29 @@
+class TodoItemsController < ApplicationController
+before_action :set_todo_list
+
+  def create
+    @todo_item = @todo_list.todo_items.create(todo_item_params)
+    redirect_to @todo_list
+  end
+
+  def destroy
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy
+      flash[:success] = "The todo list item was successfully deleted"
+    else
+      flash[:error] = "Unforunately we could not delete your todo list item"
+    end
+      redirect_to @todo_list
+  end
+
+  private
+
+  def set_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def todo_item_params
+    params[:todo_item].permit(:content)
+  end
+
+end
